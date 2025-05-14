f00cf1cacf9f1dfbf8aa21145e3356477236d053
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-05-14 18:11:50 -0500

    Practice

---
+++ using-state-informs/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import FormData from './component/FormatData'
+
+function App() {
+  return (
+    <>
+      <div className="card">
+      <h1>User Feedback Form</h1>
+        <body>
+        <FormData />
+        </body>
+      </div>
+      </>
+  )
+}
+
+export default App
---
+++ using-state-informs/src/component/FormatData.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react"
+
+function FormData(){
+    
+const [formData, setFormData]= useState({
+    name:"",
+    email:"",
+    feedback:"",
+});
+
+const handleFormData= (e) => {
+    const{ name, value} = e.target;
+
+    console.log(`Updating ${name}:`, value);
+    setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+    }));
+
+}
+
+
+return(
+    <div>
+        <form>
+            <label>
+                Name:
+                <input 
+                type="text" 
+                name="name"
+                value= {formData.name} 
+                placeholder="Enter Your Name"
+                onChange= {handleFormData} />
+            </label><br />
+            <label>
+                Email:
+                <input 
+                type="text" 
+                name="email"
+                value= {formData.email}
+                placeholder="Enter Your Email"
+                onChange= {handleFormData} />
+            </label><br />
+            <label>
+                Feedback:
+                <textarea name="feedback"
+                value= {formData.feedback}
+                placeholder="Please provide your feedback(max 200 Characters"
+                onChange= {handleFormData} rows="5" cols="33" 
+                    maxLength={200}
+                />
+                <p> {formData.feedback.length}/200 Characters</p>
+            </label>
+            <button type="submit" onClick={SubmitEvent} />
+        </form>
+        <h2>Preview</h2>
+        <p>Name: {formData.name}</p>
+        <p>Email: {formData.email}</p>
+        <p>Feedback: {formData.feedback}</p>
+    </div>
+);
+
+}
+export default FormData;
+
+
+
+
+
+
